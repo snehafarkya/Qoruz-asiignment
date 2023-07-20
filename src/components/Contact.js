e286206874925b6f2aac64f5f2d23fe57a8e5c6b
snehafarkya <snhafarkya@gmail.com>
2023-07-20 17:05:00 +0530

    updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
         <div className="card">
 <div className="data">
   <p>Don't miss out on our platform's full potential</p>
-  <button className='primary-btn'>Sign in now <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+  <button className='primary-btn contact-btn'>Sign in now <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
 <path d="M19.3605 1.49993C19.3605 1.34487 19.198 1.18981 19.0356 1.03476C18.8732 0.879697 18.7107 0.724638 18.5483 0.724638C18.3858 0.56958 18.061 0.56958 17.8985 0.56958H3.27872C2.30407 0.56958 1.6543 1.18981 1.6543 2.12016C1.6543 3.05051 2.30407 3.67075 3.27872 3.67075H13.9999L0.517203 16.5406C-0.132565 17.1608 -0.132565 18.0912 0.517203 18.7114C0.842087 19.0215 1.16697 19.1766 1.6543 19.1766C2.14162 19.1766 2.46651 19.0215 2.79139 18.7114L16.2741 5.84157V16.0754C16.2741 17.0058 16.9239 17.626 17.8985 17.626C18.8732 17.626 19.5229 17.0058 19.5229 16.0754V2.12016C19.5229 1.96511 19.5229 1.65499 19.3605 1.49993Z" fill="white"/>
 </svg></button>
 </div>
